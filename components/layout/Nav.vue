2948a568ccdc55acd222ee0e04e6008a66eafad7
Josh Stuible <joshstuible@gmail.com>
2020-06-30 13:18:00 -0700

    make nav sticky, add soft gradient behind nav

--- components/layout/Nav.vue
+++ components/layout/Nav.vue
@@ -1,43 +1,53 @@
 <template>
   <header id="top-nav">
-    <div class="container nav">
-      <logo id="nav-logo"/>
-      <a id="cta" href="">Let's Make Something</a>
-    </div>
+    <nav class="container">
+      <logo id="nav-logo" />
+      <a id="cta" href>Let's Make Something</a>
+    </nav>
   </header>
 </template>
 
 <script>
 import Logo from "~/assets/logo/logo.svg?inline";
 export default {
-    components: {
-        Logo
-    }
+  components: {
+    Logo
+  }
 };
 </script>
 
 <style lang="scss">
 header {
-    padding: 50px 0;
+  padding: 50px 0 50px 0;
+
+  @include breakpoint(thone) {
+    padding: 50px 0 150px 0;
+    background: linear-gradient(
+      0deg,
+      rgba(255, 255, 255, 0) 0%,
+      rgba(255, 255, 255, 0.9) 100%
+    );
+    position: sticky;
+    top: 0;
+  }
 }
-.nav {
-    display: flex;
-    justify-content: space-between;
-    /* flex-direction: row; */
-    
+nav {
+  display: flex;
+  justify-content: space-between;
+  /* flex-direction: row; */
 }
 #cta {
-    /* justify-self: flex-end; */
-    padding: 0.85em;
-    border: black 2px solid;
+  /* justify-self: flex-end; */
+  padding: 0.85em;
+  border: black 2px solid;
 
-    &:hover {
-        background-color: black;
-        color: white;
-    }
-    /* color: white; */
+  &:hover {
+    background-color: black;
+    color: white;
+  }
+  /* color: white; */
 }
 #nav-logo {
-    width: 25px;
+  width: 25px;
 }
 </style>
