19e1861333a5c253e74cb417f83606840a737c87
Josh Stuible <joshstuible@gmail.com>
2020-08-02 01:21:57 -0700

    Added video to top of meta logo

--- components/MetaLogo/MetaLogoTop.vue
+++ components/MetaLogo/MetaLogoTop.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="meta-logo-section">
-    <TopLogo />
+    <video
+      autoplay
+      playsinline
+      muted
+      loop
+      preload
+      poster=""
+    >
+      <source :src="require('~/assets/videos/logo-top-video.mp4')" />
+    </video>
+    <!-- <TopLogo /> -->
   </div>
 </template>
 
@@ -8,11 +18,30 @@
 import TopLogo from "~/assets/logo/metaTop.svg?inline";
 export default {
   components: {
-    TopLogo
-  }
+    TopLogo,
+  },
 };
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+// video { width: 100%; }
+// svg { 
+//   width: 100%;
+//   position:absolute;
+//   top: 0;
+//   left: 0; // needed for FF, Safari, Edge
+//   height: 100%;
+// }
+// svg text {
+//   font-family: Biko, sans-serif;
+//   font-weight: 700;
+//   text-transform: uppercase;
+//   font-size: 38px;
+// }
+// svg rect {
+//   fill: white;
+// }
+// svg > rect {
+//     mask: url(#top-logo-mask);
+// }
 </style>
