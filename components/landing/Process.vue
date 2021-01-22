a94be3c36a6c203de2e8f3084219facf3393ef17
Josh Stuible <joshstuible@gmail.com>
2021-01-21 20:37:30 -0800

    Process icons and demo copy

--- components/landing/Process.vue
+++ components/landing/Process.vue
@@ -1,31 +1,62 @@
 <template>
   <ol id="process">
-    <li>Research</li>
-    <li>Prototype</li>
-    <li>Build</li>
-    <li>Iterate</li>
+    <li v-for="step in steps" :key="step.name" class="step">
+      <component :is="step.name" class="icon" />
+      <h3>{{ step.name }}</h3>
+      <p>{{ step.description }}</p>
+    </li>
   </ol>
 </template>
 
 <script>
-export default {};
+import Research from "~/assets/images/research.svg?inline";
+import Design from "~/assets/images/design.svg?inline";
+import Build from "~/assets/images/build.svg?inline";
+import Iterate from "~/assets/images/iterate.svg?inline";
+export default {
+  components: {
+    Research,
+    Design,
+    Build,
+    Iterate,
+  },
+  data() {
+    return {
+      steps: [
+        {
+          name: "Research",
+          description:
+            "Leverage agile frameworks to provide a robust synopsis for high level overviews.",
+        },
+        {
+          name: "Design",
+          description:
+            "Leverage agile frameworks to provide a robust synopsis for high level overviews.",
+        },
+        {
+          name: "Build",
+          description:
+            "Leverage agile frameworks to provide a robust synopsis for high level overviews.",
+        },
+        {
+          name: "Iterate",
+          description:
+            "Leverage agile frameworks to provide a robust synopsis for high level overviews.",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss">
-@counter-style process {
-  system: fixed;
-  symbols: "(1) =>" "(2) =>" "(3) =>" "(4) =>" "(5) =>" "(6) =>" "(7) =>"
-    "(8) =>" "(9) =>" "(10) =>";
-  suffix: " ";
-}
-
 #process {
-  list-style: process;
-  list-style-position: inside;
+  list-style: none;
   display: grid;
   grid-template-columns: 1fr;
   font-size: 1em;
   font-weight: bold;
+  padding: 0;
 
   @include breakpoint(phablet) {
     grid-template-columns: 1fr 1fr 1fr 1fr;
@@ -33,6 +64,33 @@ export default {};
 
   @include breakpoint(tablet) {
     font-size: 1.5em;
+  }
+
+  .step {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    grid-template-rows: auto auto;
+    align-items: start;
+    gap: 0px 0px;
+    grid-template-areas:
+      "logo name"
+      "logo description";
+
+    h3 {
+      grid-area: name;
+      margin: 0;
+    }
+
+    p {
+      grid-area: description;
+      font-size: 0.5em;
+    }
+
+    .icon {
+      width: 3em;
+      grid-area: logo;
+      height: auto;
+    }
   }
 }
 </style>
