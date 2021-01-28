a9637e4d5b7106ec7f8ef4f6c618bf66b8c7e367
Josh Stuible <joshstuible@gmail.com>
2021-01-27 23:36:26 -0800

    inlcude router

--- plugins/ga.client.js
+++ plugins/ga.client.js
@@ -1,12 +1,12 @@
 import Vue from 'vue';
 import VueGtag from 'vue-gtag';
 
-export default ({ isDev }) => {
-	if (!isDev){
-		Vue.use(VueGtag, {
-			config: { id: process.env.GOOGLE_ANALYTICS_ID }
-		});
-	} else {
-		console.log("Skipping GA in development")
-	}
+export default ({ isDev, app }) => {
+    if (!isDev) {
+        Vue.use(VueGtag, {
+            config: { id: process.env.GOOGLE_ANALYTICS_ID }
+        }, app.router);
+    } else {
+        console.log("Skipping GA in development")
+    }
 }
