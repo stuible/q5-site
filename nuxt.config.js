3b12c6759f15fdcc30d22c41c86d5e94b8bf083f
Josh Stuible <1476537+stuible@users.noreply.github.com>
2021-07-19 14:47:47 -0700

    Updated to latest version of nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,7 +49,9 @@ export default {
   ** Auto import components
   ** See https://nuxtjs.org/api/configuration-components
   */
-  components: true,
+  components: [
+    { path: '~/components', pathPrefix: false }
+  ],
   /*
   ** Nuxt.js dev-modules
   */
